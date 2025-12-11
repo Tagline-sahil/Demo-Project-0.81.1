b040e0a6c111d63fcc389861a595b289565d80f7
Tagline-Sahil <sbr@taglineinfotech.com>
2025-12-11 14:53:06 +0530

    feat: Implement Firestore integration for user data display, update, and deletion within a FlatList.

--- src/screen/userDetails/UserDetailsScreen.tsx
+++ src/screen/userDetails/UserDetailsScreen.tsx
@@ -1,5 +1,13 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native';
-import React, { useRef, useState } from 'react';
+import {
+  Button,
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import firestore from '@react-native-firebase/firestore';
 
@@ -10,15 +18,28 @@ interface User {
 }
 
 const UserDetailsScreen = () => {
+  const width = Dimensions.get('window').width;
+  const [allUser, setAllUser] = useState(null);
+  console.log('🚀 ~ UserDetailsScreen ~ allUser:', allUser);
   const [name, setName] = useState('');
-  //   const [email, setEmail] = useState('');
   const [age, setAge] = useState('');
   const [mobile, setMobile] = useState('');
   const [selectValue, setSelectValue] = useState(false);
   const [selectValueId, setSelectValueId] = useState('');
+  console.log('🚀 ~ UserDetailsScreen ~ selectValueId:', selectValueId);
   const inputRef = useRef(null);
 
+  useEffect(() => {
+    const unsubscribe = firestore()
+      .collection('users')
+      .onSnapshot(querySnapshot => {
+        setAllUser(querySnapshot.docs);
+      });
+    return unsubscribe;
+  }, []);
+
   const submitData = () => {
+    console.log('🚀 ~ submitData ~ else==================>');
     firestore().collection('users').doc().set({
       name,
       age,
@@ -26,29 +47,36 @@ const UserDetailsScreen = () => {
     });
     inputRef.current?.focus();
     setName('');
-    // setEmail('');
     setAge('');
     setMobile('');
   };
 
-  //   const selectUserData = (item: User) => {
-  //     setSelectValue(true);
-  //     setSelectValueId(item.id);
-  //     setName(item.name);
-  //     setEmail(item.email);
-  //     setAge(item.age);
-  //     setMobile(item.mobile);
-  //   };
+  const selectUserData = item => {
+    setSelectValue(true);
+    setSelectValueId(item.id);
+    setName(item._data.name);
+    setAge(item._data.age);
+    setMobile(item._data.mobile);
+  };
 
   const updateUserDetails = () => {
+    firestore().collection('users').doc(selectValueId).update({
+      name,
+      age,
+      mobile,
+    });
     inputRef.current?.focus();
     setSelectValue(false);
     setSelectValueId('');
     setName('');
-    // setEmail('');
     setAge('');
     setMobile('');
   };
+
+  const deleteUSerData = id => {
+    firestore().collection('users').doc(id).delete();
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, backgroundColor: 'lightgray' }}>
@@ -60,13 +88,6 @@ const UserDetailsScreen = () => {
           style={styles.textInputStyle}
           inputMode="text"
         />
-        {/* <TextInput
-          placeholder="Enter email"
-          value={email}
-          onChangeText={setEmail}
-          style={styles.textInputStyle}
-          inputMode="email"
-        /> */}
         <TextInput
           placeholder="Enter age"
           value={age}
@@ -87,63 +108,63 @@ const UserDetailsScreen = () => {
           <Button title="Submit" onPress={submitData} />
         )}
 
-        {/* <FlatList
-        data={allUser}
-        keyExtractor={(item, index) => index.toString()}
-        ListEmptyComponent={
-          <Text
-            style={{
-              textAlign: 'center',
-              marginTop: 20,
-              fontSize: 18,
-              opacity: 0.6,
-            }}
-          >
-            No users added yet
-          </Text>
-        }
-        renderItem={({ item }) => (
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'white',
-              justifyContent: 'center',
-              alignItems: 'center',
-              paddingVertical: 10,
-              marginBottom: 5,
-              elevation: 10,
-              shadowColor: 'black',
-            }}
-          >
-            <Text style={{ fontSize: 18 }}>Name:- {item.name}</Text>
-            <Text style={{ fontSize: 18 }}>Email:- {item.email}</Text>
-            <Text style={{ fontSize: 18 }}>Age:- {item.age}</Text>
-            <Text style={{ fontSize: 18 }}>Mobile:- {item.mobile}</Text>
-            <View
+        <FlatList
+          data={allUser}
+          keyExtractor={(item, index) => index.toString()}
+          ListEmptyComponent={
+            <Text
               style={{
-                // backgroundColor: 'red',
-                height: 50,
-                width: width - 30,
-                flexDirection: 'row',
-                justifyContent: 'center',
-                gap: 10,
+                textAlign: 'center',
+                marginTop: 20,
+                fontSize: 18,
+                opacity: 0.6,
               }}
             >
-              <Button mode="contained" onPress={() => selectUserData(item)}>
-                {selectValue && item.id === selectValueId
-                  ? 'selected'
-                  : 'Update'}
-              </Button>
-              <Button
-                mode="contained"
-                onPress={() => dispatch(deleteUser(item.id))}
+              No users added yet
+            </Text>
+          }
+          renderItem={({ item }) => (
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'white',
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingVertical: 10,
+                marginVertical: 5,
+                elevation: 10,
+                shadowColor: 'black',
+              }}
+            >
+              <Text style={{ fontSize: 18 }}>Name:- {item._data.name}</Text>
+              <Text style={{ fontSize: 18 }}>Age:- {item._data.age}</Text>
+              <Text style={{ fontSize: 18 }}>Mobile:- {item._data.mobile}</Text>
+              <View
+                style={{
+                  // backgroundColor: 'red',
+                  height: 50,
+                  width: width - 30,
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  gap: 10,
+                }}
               >
-                Delete
-              </Button>
+                <Button
+                  onPress={() => selectUserData(item)}
+                  title={
+                    selectValue && item.id === selectValueId
+                      ? 'selected'
+                      : 'Update'
+                  }
+                />
+                <Button
+                  onPress={() => deleteUSerData(item.id)}
+                  title="Delete"
+                />
+              </View>
             </View>
-          </View>
-        )}
-      /> */}
+          )}
+        />
       </View>
     </SafeAreaView>
   );
